d18d856f26d2dbe116cb84fa2ee4886f195ad2ac
59939 <59939@etu.he2b.be>
2022-12-17 21:52:04 +0100

    projet final v2.0

---
+++ boulderDash/src/main/java/model/GameTest.java
@@ -0,0 +1,41 @@
+//package model;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//
+//class GameTest {
+//    public GameTest(){
+//
+//    }
+//
+//    @org.junit.jupiter.api.BeforeEach
+//    void setUp() {
+//    }
+//
+//    @org.junit.jupiter.api.AfterEach
+//    void tearDown() {
+//    }
+//
+//    @org.junit.jupiter.api.Test
+//    void isGameOver() {
+//    }
+//
+//    @org.junit.jupiter.api.Test
+//    void gameWin() {
+//    }
+//
+//    @org.junit.jupiter.api.Test
+//    void moveRockford() {
+//    }
+//
+//    @org.junit.jupiter.api.Test
+//    void moveItemsVertical() {
+//    }
+//
+//    @org.junit.jupiter.api.Test
+//    void moveItemsDiagonal() {
+//    }
+//}
